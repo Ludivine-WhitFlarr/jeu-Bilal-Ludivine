4709bef9af3e1db3e4ae8b6a2a33f3a826d9c1ee
billkarakilic <billkarakilic@gmail.com>
2018-09-18 14:57:09 +0200

    bilal-animal

--- src/js/Animal-Quiz.jsx
+++ src/js/Animal-Quiz.jsx
@@ -1,13 +1,47 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+
+import Axoloti from '../img/animaux/axolotl.jpg';
+import Emeu from '../img/animaux/emeu.jpg';
+import Narval from '../img/animaux/narval.jpg';
+import Renard from '../img/animaux/renard.jpg';
+import Requin from '../img/animaux/requin.jpg';
+import Tarsier from '../img/animaux/tarsier.jpg';
+import Tatou from '../img/animaux/tatou.jpg';
+import Quizz from './Quizz';
 
 class AnimalQuiz extends Component {
   render() {
     return (
-      <Fragment>
-      <div>
-        <p>AnimalQuiz</p>
-      </div>
-      </Fragment>
+      <Quizz title="Animal Quiz" data={[
+        {
+          img: Axoloti,
+          value: "Axoloti"
+        },
+        {
+          img: Emeu,
+          value: "Emeu"
+        },
+        {
+          img: Narval,
+          value: "Narval"
+        },
+        {
+          img: Renard,
+          value: "Renard"
+        },
+        {
+          img: Requin,
+          value: "Requin"
+        },
+        {
+          img: Tarsier,
+          value: "Tarsier"
+        },
+        {
+          img: Tatou,
+          value: "Tatou"
+        }
+      ]} />
     );
   }
 }
